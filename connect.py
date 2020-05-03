38f7d206cbfd121b729f62503941390ab5568c47
Jolene Wium <jolene@sumitins.com>
2020-05-03 15:50:08 +0200

    update path for ubuntu use

--- connect.py
+++ connect.py
@@ -12,32 +12,50 @@ import os
 
 if os.getenv('COMPUTERNAME') == 'DESKTOP-7LVDBO0':
     connect = r'C:\sumitins\connect'
+    
+    
+    open_file=open(connect + '\\password3','r')
+    file_lines=open_file.readlines()
+    sun = file_lines[0].strip()  # First Line
+    spw = file_lines[1].strip()  # Second Line
+    
+    host="127.0.0.1"
+    port="3307"
+    user=sun
+    passwd=spw
+    database="dss"
+    
+    engine_phd = create_engine('mysql://' + user + ':' + passwd + '@' + host + ':' + port + '/' + database)
+    
+    mydb = mysql.connector.connect(
+      host=host,
+      port=port,
+      user=user,
+      passwd=passwd
+    )
+    
+    open_file=open(connect + '\\password4','r')
+    file_lines=open_file.readlines()
+    sunW = file_lines[0].strip()  # First Line
+    spwW = file_lines[1].strip()  # Second Line
+    
+    engine_central = create_engine('mysql://' + sunW + ':' + spwW + '@10.16.20.5:3306/pt_admin1718')
+    
+        
 else:
-    connect = r'~/phd/connect'
+#    connect = r'~/phd/connect'
+    connect = os.path.join(os.path.expanduser('~'), 'phd', 'connect')
 
-open_file=open(connect + '\\password3','r')
-file_lines=open_file.readlines()
-sun = file_lines[0].strip()  # First Line
-spw = file_lines[1].strip()  # Second Line
-
-host="127.0.0.1"
-port="3307"
-user=sun
-passwd=spw
-database="dss"
-
-engine_phd = create_engine('mysql://' + user + ':' + passwd + '@' + host + ':' + port + '/' + database)
-
-mydb = mysql.connector.connect(
-  host=host,
-  port=port,
-  user=user,
-  passwd=passwd
-)
-
-open_file=open(connect + '\\password4','r')
-file_lines=open_file.readlines()
-sunW = file_lines[0].strip()  # First Line
-spwW = file_lines[1].strip()  # Second Line
-
-engine_central = create_engine('mysql://' + sunW + ':' + spwW + '@10.16.20.5:3306/pt_admin1718')
+    open_file=open(connect + '\\password3','r')
+    file_lines=open_file.readlines()
+    sun = file_lines[0].strip()  # First Line
+    spw = file_lines[1].strip()  # Second Line
+    
+    host="127.0.0.1"
+    port="3306"
+    user=sun
+    passwd=spw
+    database="dss"
+    
+    engine_phd = create_engine('mysql://' + user + ':' + passwd + '@' + host + ':' + port + '/' + database)
+    
