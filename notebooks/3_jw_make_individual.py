f2432204441dcedf80edd0dd118ed8b8e1b1739d
Jolene Wium <jolene@sumitins.com>
2021-04-27 06:02:17 +0200

    before large update in 'individual' notebook

--- notebooks/3_jw_make_individual.py
+++ notebooks/3_jw_make_individual.py
@@ -21,6 +21,11 @@ pc_df = pd.DataFrame.from_dict(pc_dic, orient='index')
 pc_df['id'] = pc_df.index
 ft_df = options.from_to()
 
+individual = {}
+individuald_he = []
+individuald_pc = []
+individuald_kg = []
+test_table = pd.DataFrame()
 for d in dlist_allocate:
     print(d)
     print(ddic_he[d])
@@ -39,61 +44,69 @@ for d in dlist_allocate:
                 ddic_he[d].remove(he)  # remove he from list to not reuse it
                 continue
 
+            # Calculate kg
+            if he_dic[he]['kg_rem'] > dkg:
+                # Get the kg that will be packed
+                to_pack = dkg
+                # Subtract demand from he
+                he_dic[he]['kg_rem'] = he_dic[he]['kg_rem'] - dkg
+                dkg = 0
+                
+            else:
+                # Get the kg that will be packed
+                to_pack = he_dic[he]['kg_rem']
+                # Subtract he from demand
+                dkg = dkg - he_dic[he]['kg_rem']
+                he_dic[he]['kg_rem'] = 0
+                ddic_he[d].remove(he)
+
+            # Get closest pc for he from available pc's
+            block_id = he_dic[he]['block_id']
+            
+            # Variables to determine speed -> add calculate the number of hours 
+            packtype_id = ddic_metadata[d]['pack_type_id']
+            ft_dft = ft_df[ft_df['block_id'] == block_id]
+
+            # Allocate to_pack to pack capacities
+            while to_pack > 0:
+                pc_dft = pc_df[pc_df['time_id'] == ddic_metadata[d]['time_id']]
+                pc_dft = pc_dft[pc_dft['pack_type_id'] == packtype_id]
+                pc_dft = pc_dft[pc_dft['kg_rem'] > 0]
+
+                if len(ft_dft) > 0 and len(pc_dft):
+                    pc_dft = pc_dft.merge(ft_dft, on='packhouse_id', how='left')
+                    pcf_dft = pc_dft.sort_values(['km']).reset_index(drop=True)
+
+                    # Allocate closest pc to block
+                    pc = pcf_dft.id[0]
+                    packhouse_id = pcf_dft.packhouse_id[0]
+                    km = pcf_dft.km[0]
+                    pckg_rem = pcf_dft.kg_rem[0]
+
+                    if pckg_rem > to_pack:
+                        packed = to_pack
+                        pckg_rem = pckg_rem - to_pack
+                        to_pack = 0
+
+                    else:
+                        packed = pckg_rem
+                        to_pack = to_pack - pckg_rem
+                        pckg_rem = 0
+
+                    pc_df.loc[(pc_df['id'] == pc), 'kg_rem'] = pckg_rem
+
+                    individuald_he.append(he)
+                    individuald_pc.append(pc)
+                    individuald_kg.append(packed)
+
+                else:
+                    break
+                
+            individual.update({d:{'he':individuald_he, 'pc':individuald_pc}})        
+        
         else:
             break
 
-        # Calculate kg
-        if he_dic[he]['kg_rem'] > dkg:
-            # Get the kg that will be packed
-            to_pack = dkg
-            # Subtract demand from he
-            he_dic[he]['kg_rem'] = he_dic[he]['kg_rem'] - dkg
-            dkg = 0
-            
-        else:
-            # Get the kg that will be packed
-            to_pack = he_dic[he]['kg_rem']
-            # Subtract he from demand
-            dkg = dkg - he_dic[he]['kg_rem']
-            he_dic[he]['kg_rem'] = 0
-            ddic_he[d].remove(he)
 
-
-        # Get closest pc for he from available pc's
-        block_id = he_dic[he]['block_id']
-        
-        # Variables to determine speed  -> add calculate the number of hours 
-        va_id = he_dic[he]['va_id']
-        packtype_id = ddic_metadata[d]['pack_type_id']
-        
-        ft_dft = ft_df[ft_df['block_id'] == block_id]
-
-        # Allocate to_pack to pack capacities
-        while to_pack > 0:
-            pc_dft = pc_df[pc_df['time_id'] == ddic_metadata[d]['time_id']]
-            pc_dft = pc_dft[pc_dft['pack_type_id'] == packtype_id]
-            pc_dft = pc_dft[pc_dft['kg_rem'] > 0]
-
-            if len(ft_dft) > 0 and len(pc_dft):
-                pc_dft = pc_dft.merge(ft_dft, on='packhouse_id', how='left')
-                pcf_dft = pc_dft.sort_values(['km']).reset_index(drop=True)
-
-                # allocate closest pc to block
-                dhe_pc = pcf_dft.id[0]
-                packhouse_id = pcf_dft.packhouse_id[0]
-                km = pcf_dft.km[0]
-                pckg_rem = pcf_dft.kg_rem[0]
-
-                if pckg_rem > to_pack:
-                    pckg_rem = pckg_rem - to_pack
-                    to_pack = 0
-
-                else:
-                    to_pack = to_pack - pckg_rem
-                    pckg_rem = 0
-
-
-            else:
-                break
-            
-            
+# TODO only subtract kg from he & demand if it can be packed!!!!!!!!!!
+# TODO update pc_df
